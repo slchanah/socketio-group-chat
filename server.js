cfef267614ea4f158021a9b59b186bfd7d955213
Ivan Chan <ivan.aritaone@gmail.com>
2021-02-19 21:48:54 +0800

    sokcetio server done

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-const { v4: uuidv4 } = require('uuid');
 const cors = require('cors');
 
 const app = express();
@@ -19,7 +18,45 @@ app.use(
   })
 );
 
-io.on('connection', (socket) => {});
+io.on('connection', (socket) => {
+  socket.on('validate-roomId', (ROOM_ID) => {
+    const isExisted = io.sockets.adapter.rooms.get(ROOM_ID);
+
+    if (isExisted) {
+      socket.emit('validated-roomId', ROOM_ID);
+    } else {
+      socket.emit('validated-roomId');
+    }
+  });
+
+  socket.on('join-room', (ROOM_ID) => {
+    const clientIds = Array.from(io.sockets.adapter.rooms.get(ROOM_ID) || []);
+
+    if (clientIds.length >= 2) {
+      socket.emit('room-full');
+    } else {
+      socket.join(ROOM_ID);
+      socket.emit('all-socket-ids', clientIds);
+    }
+  });
+
+  socket.on('send-signal', (peerId, callerId, signal) => {
+    io.to(peerId).emit('user-connected', callerId, signal);
+  });
+
+  socket.on('answer-signal', (answerId, callerId, signal) => {
+    io.to(callerId).emit('returning-signal', answerId, signal);
+  });
+
+  socket.on('user-disconnect', (socketId, ROOM_ID) => {
+    socket.to(ROOM_ID).emit('destroy-peer', socketId);
+    socket.leave(ROOM_ID);
+  });
+
+  socket.on('send-message', (username, message, ROOM_ID) => {
+    socket.to(ROOM_ID).emit('receive-message', username, message);
+  });
+});
 
 server.listen(3030, () => {
   console.log('Server starts');
